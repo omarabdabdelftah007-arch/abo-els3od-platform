cc922e69201e21d9e544c5003dccce39d77b655e
omarabdabdelftah007-arch <omar.abdabdelftah007@mail.com>
2026-08-18 06:03:37 +0300

    Add sitemap.xml URL pattern

--- core/el_saoud_platform/urls.py
+++ core/el_saoud_platform/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # ده السطر اللي هيربط الموقع بتطبيق core
+    
+    # مسار خريطة الموقع الـ SEO
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type="text/xml")),
+
+    # ربط الموقع بتطبيق core
     path('', include('core.urls')), 
 ]
